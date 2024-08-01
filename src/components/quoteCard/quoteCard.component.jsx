30422a479055bd10ca98bc4406977bd00ffa00b1
mrpelumi <oguntolapelumi6@gmail.com>
2024-08-01 00:40:26 +0000

    quote card adjusted

--- src/components/quoteCard/quoteCard.component.jsx
+++ src/components/quoteCard/quoteCard.component.jsx
@@ -15,7 +15,7 @@ const QuoteCard = () => {
   }, [])
 
   return (
-    <div className='xl:w-3/5 md:w-4/5 flex flex-col xl:p-4 p-6 items-center bg-slate-800 text-white rounded-md gap-4'>
+    <div className='w-full xl:w-3/5 md:w-4/5 flex flex-col xl:p-4 p-6 items-center bg-slate-800 text-white rounded-md gap-4'>
       <div>
         <span className='text-xl'>Quote of the Week</span>
       </div>
